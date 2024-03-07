4f718d13420fb1d77c5817d395a62907c4f57de7
whitecat1331 <whitecat1331@gmail.com>
2024-03-06 20:38:33 -0500

    Inital Commit

---
+++ pycon.py
@@ -0,0 +1,61 @@
+import sublist3r
+import dns.resolver
+from icecream import ic
+from pythonping import ping
+
+class Pycon:
+    DNS_RECORD_TYPES = ['A', 'AAAA', 'MX', 'NS', 'TXT', 'SOA']
+    def __init__(self, dmoain):
+        self.root_domain = domain
+        self.subdomains = []
+
+    def get_subdomains(self):
+        # sublist3r
+        # knock
+        pass
+
+    @staticmethod
+    def query_sublist3r(domain, no_threads=40, savefile=None,
+                      ports=None, silent=True, verbose=False, 
+                      enable_bruteforce=False, engines=None):
+        return sublist3r.main(domain, no_threads, savefile, ports, silent, 
+                              verbose, enable_bruteforce, engines)
+
+    @staticmethod
+    def query_dns(domain):
+        info = {"domain": domain}
+        for qtype in Pycon.DNS_RECORD_TYPES:
+            answer = dns.resolver.resolve(domain, qtype, raise_on_no_answer=False)
+            if answer.rrset is not None:
+                info[qtype] = answer.rrset
+
+        return info
+
+    @staticmethod
+    def is_alive(host, count=3, timeout=2):
+        ping_result = ping(target=host, count=count, timeout=timeout)
+        return ping_result.stats_packets_returned > 0
+
+    @staticmethod
+    def has_http(domain):
+        return  200 <= requests.get(f"http://{domain}").status_code < 300
+
+    @staticmethod
+    def has_https(domain):
+        return  200 <= requests.get(f"https://{domain}").status_code < 300
+
+    def check_takeover():
+        pass
+
+        
+
+def main():
+    pass
+
+def test(domain="amazon.com"):
+    # ic(Pycon.query_sublist3r(domain))
+    # ic(Pycon.query_dns(domain))
+    ic(Pycon.ping_host(domain))
+
+if __name__ == "__main__":
+    test()
